4eb0225dea4d3abe4aea14853eb2c948dc80cda9
dhananjay907 <dhananjaysharma9070@gmail.com>
2022-05-23 21:38:48 -0400

    Class modified : SportsDatamodel

--- src/main/java/com/example/lab1/SportDatamodel.java
+++ src/main/java/com/example/lab1/SportDatamodel.java
@@ -2,5 +2,39 @@ package com.example.lab1;
 
 public class SportDatamodel {
 
+    private String sportName;
+    private Integer numOfplayers;
+    private String jersyColor;
+
+
+    public SportDatamodel(String sportName, Integer numOfplayers, String jersyColor) {
+        this.sportName = sportName;
+        this.numOfplayers = numOfplayers;
+        this.jersyColor = jersyColor;
+    }
+
+    public String getSportName() {
+        return sportName;
+    }
+
+    public void setSportName(String sportName) {
+        this.sportName = sportName;
+    }
+
+    public Integer getNumOfplayers() {
+        return numOfplayers;
+    }
+
+    public void setNumOfplayers(Integer numOfplayers) {
+        this.numOfplayers = numOfplayers;
+    }
+
+    public String getJersyColor() {
+        return jersyColor;
+    }
+
+    public void setJersyColor(String jersyColor) {
+        this.jersyColor = jersyColor;
+    }
 
 }
